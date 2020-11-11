31151854f4b40d081a64359e5024962da21dac09
Denis <denis.andreevir@gmail.com>
2020-11-11 15:12:11 +0300

    create class group and student

---
+++ src/main/java/ru/andreev/gradebook/entity/Group.java
@@ -0,0 +1,36 @@
+package ru.andreev.gradebook.entity;
+
+import java.util.Set;
+
+public class Group {
+    private Integer id;
+    private String name;
+    private Set<Student> students;
+
+    public Group() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+}
---
+++ src/main/java/ru/andreev/gradebook/entity/Student.java
@@ -0,0 +1,44 @@
+package ru.andreev.gradebook.entity;
+
+public class Student {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    private Group group;
+
+    public Student() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+}
